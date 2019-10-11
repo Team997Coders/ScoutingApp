ef23e0a6943640c74e006b4d6888620eaf268ccc
KyroVibe <hunter.barclay18@gmail.com>
2019-10-10 17:51:00 -0700

    More front end

--- app/src/main/java/org/team997/spartanscout/DeepSpace.java
+++ app/src/main/java/org/team997/spartanscout/DeepSpace.java
@@ -13,14 +13,16 @@ public class DeepSpace extends AppCompatActivity {
     private int
             autoHabScore = 0,
             cargoScored = 0,
-            hatchScored = 0;
+            hatchScored = 0,
+            climbScore = 0;
 
     private Button
             autoHabPlus, autoHabMinus,
             cargoPlus, cargoMinus,
-            hatchPlus, hatchMinus;
+            hatchPlus, hatchMinus,
+            climbPlus, climbMinus;
 
-    private TextView autoHabText, cargoText, hatchText;
+    private TextView autoHabText, cargoText, hatchText, climbText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,7 @@ public class DeepSpace extends AppCompatActivity {
         autoHabText = (TextView) findViewById(R.id.autoHabText);
         cargoText = (TextView) findViewById(R.id.cargoText);
         hatchText = (TextView) findViewById(R.id.hatchText);
+        climbText = (TextView) findViewById(R.id.climbText);
 
         autoHabPlus = (Button) findViewById(R.id.autoHabPlus);
         autoHabPlus.setOnClickListener(new View.OnClickListener() {
@@ -38,13 +41,13 @@ public class DeepSpace extends AppCompatActivity {
                 switch (autoHabScore) {
                     case 0:
                         autoHabScore = 3;
+                        autoHabText.setText("Auto Hab\nLevel 1");
                         break;
                     case 3:
                         autoHabScore = 6;
+                        autoHabText.setText("Auto Hab\nLevel 2");
                         break;
                 }
-
-                autoHabText.setText("Auto Hab\n" + autoHabScore);
             }
         });
         autoHabMinus = (Button) findViewById(R.id.autoHabMinus);
@@ -54,13 +57,13 @@ public class DeepSpace extends AppCompatActivity {
                 switch (autoHabScore) {
                     case 6:
                         autoHabScore = 3;
+                        autoHabText.setText("Auto Hab\nLevel 1");
                         break;
                     case 3:
                         autoHabScore = 0;
+                        autoHabText.setText("Auto Hab\nDidn't Score");
                         break;
                 }
-
-                autoHabText.setText("Auto Hab\n" + autoHabScore);
             }
         });
         cargoPlus = (Button) findViewById(R.id.cargoPlus);
@@ -94,6 +97,46 @@ public class DeepSpace extends AppCompatActivity {
             public void onClick(View view) {
                 if (hatchScored > 0) hatchScored -= 1;
                 hatchText.setText("Hatches\n" + hatchScored);
+            }
+        });
+        climbPlus = (Button) findViewById(R.id.climbPlus);
+        climbPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (climbScore) {
+                    case 0:
+                        climbScore = 3;
+                        climbText.setText("Climb\nLevel 1");
+                        break;
+                    case 3:
+                        climbScore = 6;
+                        climbText.setText("Climb\nLevel 2");
+                        break;
+                    case 6:
+                        climbScore = 12;
+                        climbText.setText("Climb\nLevel 3");
+                        break;
+                }
+            }
+        });
+        climbMinus = (Button) findViewById(R.id.climbMinus);
+        climbMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (climbScore) {
+                    case 12:
+                        climbScore = 6;
+                        climbText.setText("Climb\nLevel 2");
+                        break;
+                    case 6:
+                        climbScore = 3;
+                        climbText.setText("Climb\nLevel 1");
+                        break;
+                    case 3:
+                        climbScore = 0;
+                        climbText.setText("Climb\nNo Climb");
+                        break;
+                }
             }
         });
     }
