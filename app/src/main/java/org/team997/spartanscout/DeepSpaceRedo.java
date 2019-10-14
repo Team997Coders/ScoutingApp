1ed021a2423233a5065a0a1375f2c4549c35a7dd
KyroVibe <hunter.barclay18@gmail.com>
2019-10-13 20:46:18 -0700

    First deepspace layout complete. Now I need to start work on the software to parse the data and completition of the backend of the app

--- app/src/main/java/org/team997/spartanscout/DeepSpaceRedo.java
+++ app/src/main/java/org/team997/spartanscout/DeepSpaceRedo.java
@@ -10,9 +10,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.team997coders.spartanlib.scouting.DeepSpaceData;
+
 public class DeepSpaceRedo extends AppCompatActivity {
 
+    private int match = 0, team = 0;
+    private DeepSpaceData.Color alliance = DeepSpaceData.Color.Red;
+
     private int unselTextColor, unselTintColor, selTextColor, selTintColor;
+
+    private boolean limited = false;
 
     private int lowCargo = 0, midCargo = 0, highCargo = 0, csCargo = 0,
             lowHatch = 0, midHatch = 0, highHatch = 0,
@@ -22,11 +29,13 @@ public class DeepSpaceRedo extends AppCompatActivity {
             lowCargoM, midCargoM, highCargoM, csCargoM,
             lowHatchP, midHatchP, highHatchP,
             lowHatchM, midHatchM, highHatchM,
+            penaltyP, penaltyM,
             autoHab0, autoHab1, autoHab2,
-            climb0, climb1, climb2, climb3;
+            defense0, defense1, defense2, defense3,
+            climb0, climb1, climb2, climb3, limitedToggle, allianceToggle;
 
     private TextView lowCargoT, midCargoT, highCargoT, csCargoT,
-            lowHatchT, midHatchT, highHatchT;
+            penaltyT, lowHatchT, midHatchT, highHatchT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,36 @@ public class DeepSpaceRedo extends AppCompatActivity {
         selTextColor = Color.argb(255, 46, 174, 217); // 783211007;
         unselTintColor = 14079959;
         unselTextColor = Color.argb(255, 35, 35, 35); // 589505535;
+
+        // Togggggggglllllleeee
+
+        allianceToggle = (Button) findViewById(R.id.allianceToggle);
+        allianceToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (alliance == DeepSpaceData.Color.Blue) {
+                    alliance = DeepSpaceData.Color.Red;
+                    allianceToggle.setText("RED");
+                } else {
+                    alliance = DeepSpaceData.Color.Blue;
+                    allianceToggle.setText("BLUE");
+                }
+            }
+        });
+
+        limitedToggle = (Button) findViewById(R.id.limitedToggle);
+        limitedToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (limited) {
+                    limited = false;
+                    limitedToggle.setText("No");
+                } else {
+                    limited = true;
+                    limitedToggle.setText("Yes");
+                }
+            }
+        });
 
         // Cargo Frontend
 
@@ -212,6 +251,139 @@ public class DeepSpaceRedo extends AppCompatActivity {
                     // autoHab1.setBackgroundTintList(ColorStateList.valueOf(unselTintColor));
                     autoHab0.setTextColor(unselTextColor);
                     // autoHab0.setBackgroundTintList(ColorStateList.valueOf(unselTintColor));
+                }
+            }
+        });
+
+        // Penalty
+
+        penaltyT = (TextView) findViewById(R.id.penaltyText);
+
+        penaltyP = (Button) findViewById(R.id.penaltyPlus);
+        penaltyP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (penalties < 99) penalties++;
+                penaltyT.setText("Fouls Incurred: " + penalties);
+            }
+        });
+        penaltyM = (Button) findViewById(R.id.penaltyMinus);
+        penaltyM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (penalties > 0) penalties--;
+                penaltyT.setText("Fouls Incurred: " + penalties);
+            }
+        });
+
+        // Defense Stuff
+
+        defense0 = (Button) findViewById(R.id.defense0);
+        defense1 = (Button) findViewById(R.id.defense1);
+        defense2 = (Button) findViewById(R.id.defense2);
+        defense3 = (Button) findViewById(R.id.defense3);
+
+        defense0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (defense != 0) {
+                    defense = 0;
+                    defense0.setTextColor(selTextColor);
+                    defense1.setTextColor(unselTextColor);
+                    defense2.setTextColor(unselTextColor);
+                    defense3.setTextColor(unselTextColor);
+                }
+            }
+        });
+        defense1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (defense != 1) {
+                    defense = 1;
+                    defense1.setTextColor(selTextColor);
+                    defense0.setTextColor(unselTextColor);
+                    defense2.setTextColor(unselTextColor);
+                    defense3.setTextColor(unselTextColor);
+                }
+            }
+        });
+        defense2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (defense != 2) {
+                    defense = 2;
+                    defense2.setTextColor(selTextColor);
+                    defense1.setTextColor(unselTextColor);
+                    defense0.setTextColor(unselTextColor);
+                    defense3.setTextColor(unselTextColor);
+                }
+            }
+        });
+        defense3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (defense != 3) {
+                    defense = 3;
+                    defense3.setTextColor(selTextColor);
+                    defense1.setTextColor(unselTextColor);
+                    defense2.setTextColor(unselTextColor);
+                    defense0.setTextColor(unselTextColor);
+                }
+            }
+        });
+
+        // Climb
+
+        climb0 = (Button) findViewById(R.id.climb0);
+        climb1 = (Button) findViewById(R.id.climb1);
+        climb2 = (Button) findViewById(R.id.climb2);
+        climb3 = (Button) findViewById(R.id.climb3);
+
+        climb0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (climb != 0) {
+                    climb = 0;
+                    climb0.setTextColor(selTextColor);
+                    climb1.setTextColor(unselTextColor);
+                    climb2.setTextColor(unselTextColor);
+                    climb3.setTextColor(unselTextColor);
+                }
+            }
+        });
+        climb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (climb != 1) {
+                    climb = 1;
+                    climb1.setTextColor(selTextColor);
+                    climb0.setTextColor(unselTextColor);
+                    climb2.setTextColor(unselTextColor);
+                    climb3.setTextColor(unselTextColor);
+                }
+            }
+        });
+        climb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (climb != 2) {
+                    climb = 2;
+                    climb2.setTextColor(selTextColor);
+                    climb0.setTextColor(unselTextColor);
+                    climb1.setTextColor(unselTextColor);
+                    climb3.setTextColor(unselTextColor);
+                }
+            }
+        });
+        climb3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (climb != 3) {
+                    climb = 3;
+                    climb3.setTextColor(selTextColor);
+                    climb0.setTextColor(unselTextColor);
+                    climb2.setTextColor(unselTextColor);
+                    climb1.setTextColor(unselTextColor);
                 }
             }
         });
